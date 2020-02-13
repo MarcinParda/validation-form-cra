1e892a6fdf3887e1b82caafd70ea4e00b97d32a2
Marcin Parda <marcin98parda@wp.pl>
2020-02-13 17:31:17 +0100

    add controlled form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    username: "",
+    email: "",
+    pass: "",
+    accept: false
+  };
+
+  handleChange = e => {
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "email" || type === "password") {
+      const value = e.target.value;
+      this.setState({
+        [name]: value
+      });
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      });
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">
+            Twoje imię:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="email">
+            Twój e-mail:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="password">
+            Twoje hasło:
+            <input
+              type="password"
+              id="password"
+              name="pass"
+              value={this.state.pass}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />
+            Wyrażam zgodę na przetwarzanie danych
+          </label>
+
+          <button>Zapisz się</button>
+        </form>
       </div>
     );
   }
